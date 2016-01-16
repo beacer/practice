0f814491e662da85faf84ebc9876fd36747f1f70
beacer <beacer_chen@hotmail.com>
2016-01-16 16:06:51 +0800

    [netfilter] xtables ipaddr for test

---
+++ netfilter/xtables/kernel/xt_ipaddr.c
@@ -0,0 +1,106 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/skbuff.h>
+#include <linux/kernel.h>
+#include <linux/netfilter/x_tables.h>
+#include <net/ipv6.h>
+#include "xt_ipaddr.h"
+
+MODULE_AUTHOR("beacer <beacer@example.com>");
+MODULE_DESCRIPTION("xtables: match source/destination address");
+MODULE_LICENSE("GPL");
+MODULE_ALIAS("ip6t_ipaddr");
+
+static bool ipaddr_mt(const struct sk_buff *skb, struct xt_action_param *param)
+{
+    const struct xt_ipaddr_mtinfo *info = param->matchinfo; /* from userspace */
+    const struct ipv6hdr *ip6h = ipv6_hdr(skb);
+
+    /*
+    pr_info("xt_ipaddr: IN=%s OUT=%s SRC=" NIP6_FMT " DST=" NIP6_FMT " "
+            "IPSRC=" NIP6_FMT " IPDST=" NIP6_FMT "\n",
+            param->in ? param->in->name : "",
+            param->out ? param->out->name : "",
+            NIP6(ip6h->saddr), NIP6(ip6h->daddr),
+            NIP6(info->src), NIP6(info->dst));
+            */
+
+    pr_info("xt_ipaddr: IN=%s OUT=%s",
+            param->in ? param->in->name : "",
+            param->out ? param->out->name : "");
+
+    if (info->flags & XT_IPADDR_SRC) {
+        if ((ipv6_addr_cmp(&ip6h->saddr, &info->src.in6) != 0) 
+                || (info->flags & XT_IPADDR_SRC_INV)) {
+            pr_notice("src IP - no match\n");
+            return false;
+        }
+    }
+
+    if (info->flags & XT_IPADDR_DST) {
+        if ((ipv6_addr_cmp(&ip6h->daddr, &info->dst.in6) != 0) 
+                || (info->flags & XT_IPADDR_DST_INV)) {
+            pr_notice("dst IP - no match\n");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static int ipaddr_mt_check(const struct xt_mtchk_param *param)
+{
+    const struct xt_ipaddr_mtinfo *info = param->matchinfo;
+
+    pr_info("new rule with `-m ipaddr' in table %s hooks 0x%x\n",
+            param->table, param->hook_mask);
+
+    if (!(info->flags & (XT_IPADDR_SRC | XT_IPADDR_DST))) {
+        pr_info("nothing for test\n");
+        return -EINVAL;
+    }
+
+    /* sanity check*/
+    if (ntohl(info->src.ip6[0]) == 0x20010DB8) {
+        pr_info("I'm sorry, can't test 2001:db8::/23\n");
+        return -EPERM;
+    }
+
+    return 0;
+}
+
+static void ipaddr_mt_destroy(const struct xt_mtdtor_param *param)
+{
+    //const struct xt_ipaddr_mtinfo *info = param->matchinfo;
+
+    //pr_info("match for addr " NIP6_FMT " removed\n", NIP6(info->src.ip6));
+    pr_info("match for addr removed\n");
+    return;
+}
+
+static struct xt_match ipaddr_mt_reg __read_mostly = {
+    .name       = "ipaddr",             /* for 'ip6tables ... -m ipaddr ...' */
+    .revision   = 0,
+    .family     = NFPROTO_IPV6,
+    .match      = ipaddr_mt,            /* match or not */
+    .checkentry = ipaddr_mt_check,      /* before insert the rule */
+    .destroy    = ipaddr_mt_destroy,    /* when revmove the rule */
+
+    .matchsize  = sizeof(struct xt_ipaddr_mtinfo), /* userspace info size */
+    .me         = THIS_MODULE,
+};
+
+static int __init ipaddr_mt_init(void)
+{
+    pr_info("xt_ipaddr init\n");
+    return xt_register_match(&ipaddr_mt_reg);
+}
+
+static void __exit ipaddr_mt_exit(void)
+{
+    pr_info("xt_ipaddr exit\n");
+    return xt_unregister_match(&ipaddr_mt_reg);
+}
+
+module_init(ipaddr_mt_init);
+module_exit(ipaddr_mt_exit);
---
+++ netfilter/xtables/kernel/xt_ipaddr.h
@@ -0,0 +1,19 @@
+#ifndef _LINUX_NETFILTER_XT_IPADDR_H
+#define _LINUX_NETFILTER_XT_IPADDR_H 1
+#include <linux/netfilter.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+
+enum {
+    XT_IPADDR_SRC       = 0x1<<0,
+    XT_IPADDR_DST       = 0x1<<1,
+    XT_IPADDR_SRC_INV   = 0x1<<2,   /* need XT_IPADDR_SRC */
+    XT_IPADDR_DST_INV   = 0x1<<3,   /* need XT_IPADDR_DST */
+};
+
+struct xt_ipaddr_mtinfo {
+    union nf_inet_addr src, dst;    /* support ipv4/ipv6 */
+    __u8 flags;
+};
+
+#endif /* _LINUX_NETFILTER_XT_IPADDR_H */
